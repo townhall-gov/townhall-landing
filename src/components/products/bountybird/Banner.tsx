0185e8ac5ae700e802b5ce143c802198c943ce2f
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2024-02-29 13:36:33 +0530

    feat/product-bountyMb

--- src/components/products/bountybird/Banner.tsx
+++ src/components/products/bountybird/Banner.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <section className="flex flex-col gap-8 justify-between items-center w-full mb-24 px-24">
-      <div className="relative h-[70vh]  w-full ">
+    <section className="flex flex-col gap-4 md:gap-8 justify-between items-center w-full mb-8 md:mb-24 px-4 md:px-8 lg:px-24">
+      <div className="relative h-[20vh] sm:h-[40vh] lg:h-[70vh]  w-full ">
         <Image
           className="object-contain"
           src="/assets/bountybirdBanner3.png"
@@ -14,7 +14,7 @@ const Banner = () => {
           loading="lazy"
         />
       </div>
-      <Button className="fixed bottom-10 audiowide-bold z-10 text-white" size="lg" variant={"destructive"}>
+      <Button className="md:fixed bottom-10 audiowide-bold z-10 text-white" size="lg" variant={"destructive"}>
         Launch App{" "}
       </Button> 
     </section>
