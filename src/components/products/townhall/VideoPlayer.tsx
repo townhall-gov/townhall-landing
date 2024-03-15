668d5902d07cfcb65d7c2b388d38df7a2d5d2e19
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2024-03-15 17:23:24 +0530

    fix/ChangesVideoFile

--- src/components/products/townhall/VideoPlayer.tsx
+++ src/components/products/townhall/VideoPlayer.tsx
@@ -1,48 +1,63 @@
-
-"use client"
+'use client';
 
 import Image from 'next/image';
 import { useState, useRef } from 'react';
 
 const VideoPlayer: React.FC = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const [isPlaying, setIsPlaying] = useState(false);
 
-  const togglePlay = () => {
-    if (videoRef.current) {
-      if (isPlaying) {
-        videoRef.current.pause();
-      } else {
-        videoRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    }
-  };
+	const togglePlay = () => {
+		if (videoRef.current) {
+			if (isPlaying) {
+				videoRef.current.pause();
+			} else {
+				videoRef.current.play();
+			}
+			setIsPlaying(!isPlaying);
+		}
+	};
 
-  return (
-    <div className="relative mx-8 md:mx-24 rounded-3xl mt-8 md:mt-24 overflow-hidden">
-      <video
-        ref={videoRef}
-        className="w-full min-h-[250px] sm:min-h-[350px] md:min-h-[600px] object-contain"
-        controls={true}
-        // onClick={togglePlay}
-      >
-        <source src="/assets/townhallVideo.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+	return (
+		<div className="relative mx-8 md:mx-44 rounded-3xl mt-8 md:mt-24 overflow-hidden">
+			<video
+				ref={videoRef}
+				className="w-full min-h-[250px] sm:min-h-[300px] md:min-h-[400px] object-contain"
+				controls={true}
+				// onClick={togglePlay}
+			>
+				<source src="/assets/townhall-video.mp4" type="video/mp4" />
+				{/* <source
+					src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+					type="video/mp4"
+				/> */}
+				Your browser does not support the video tag.
+			</video>
 
-      <button
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 cursor-pointer flex items-center focus:outline-none"
-        onClick={togglePlay}
-      >
-        {isPlaying ? (
-          <Image className='-mt-12 opacity-0' src="/assets/PlayVideo.svg" alt="Play" width={64} height={64} />
-        ) : (
-          <Image className='-mt-12' src="/assets/PlayVideo.svg" alt="Play" width={64} height={64} />
-        )}
-      </button>
-    </div>
-  );
+			<button
+				className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 cursor-pointer flex items-center focus:outline-none"
+				onClick={togglePlay}
+			>
+				{isPlaying ? (
+					<Image
+						className="-mt-12 opacity-0"
+						src="/assets/PlayVideo.svg"
+						alt="Play"
+						width={64}
+						height={64}
+					/>
+				) : (
+					<Image
+						className="-mt-12"
+						src="/assets/PlayVideo.svg"
+						alt="Play"
+						width={64}
+						height={64}
+					/>
+				)}
+			</button>
+		</div>
+	);
 };
 
 export default VideoPlayer;
