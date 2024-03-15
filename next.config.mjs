bdab42b9281af1ae1f0ac097d583b95b25a1aff0
Nikhil Ranjan <niklabh811@gmail.com>
2024-03-15 19:16:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-};
+
+const nextConfig = {output: 'export', images: {unoptimized: true},experimental: {images: {unoptimized: true}},basePath: "/townhall-landing"};
 
 export default nextConfig;
